272749d33cfec14e55e437d9bbb5ae545cef9cf5
Keerthan <keerthangowdas222@gmail.com>
2026-01-06 03:24:20 +0530

    Revert "fix8"
    This reverts commit 3b200789d6e6aabf5d5cb2d9a7799f224cff958e.

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,7 +9,6 @@ import WalkinsCenter from "@/pages/walkins-center";
 import AuthPage from "@/pages/auth-page";
 import { ProtectedRoute } from "./lib/protected-route";
 import { AuthProvider } from "@/hooks/use-auth";
-import { ErrorBoundary } from "@/components/ErrorBoundary";
 
 function Router() {
   return (
@@ -24,17 +23,14 @@ function Router() {
 
 function App() {
   return (
-    <ErrorBoundary>
     <QueryClientProvider client={queryClient}>
-      <WouterRouter>
-        <AuthProvider>
-          <TooltipProvider>
-            <Toaster />
-          </TooltipProvider>
-        </AuthProvider>
-      </WouterRouter>
+      <AuthProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </AuthProvider>
     </QueryClientProvider>
-    </ErrorBoundary>
   );
 }
 
